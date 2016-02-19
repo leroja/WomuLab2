fa3d1a680392c99502e1d7e876c6471b211450f9
Leroja <len.nart@hotmail.com>
2016-02-19 11:05:44 +0100

    init

---
+++ web site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web_site.WebApiApplication" Language="C#" %>
---
+++ web site/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(web_site.Startup))]
+
+namespace web_site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
